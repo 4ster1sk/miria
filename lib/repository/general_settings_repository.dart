05cd5eac64235f83e9eaed2d37fd5420c9d837ac
4sterisk <4sterisk@ymail.ne.jp>
2024-04-01 04:47:19 +0900

    初回起動時に言語設定

--- lib/repository/general_settings_repository.dart
+++ lib/repository/general_settings_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:miria/model/general_settings.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -8,10 +9,22 @@ class GeneralSettingsRepository extends ChangeNotifier {
   var _settings = const GeneralSettings();
   GeneralSettings get settings => _settings;
 
+  void _initLanguage() {
+    Languages lang = Languages.jaJP;
+    switch (WidgetsBinding.instance.platformDispatcher.locale.languageCode) {
+      case "zh":
+        lang = Languages.zhCN;
+        break;
+    }
+    _settings = _settings.copyWith(languages: lang);
+  }
+
   Future<void> load() async {
     final prefs = await SharedPreferences.getInstance();
     final storedData = prefs.getString("general_settings");
     if (storedData == null || storedData.isEmpty) {
+      _initLanguage();
+      _save();
       return;
     }
 
@@ -26,7 +39,11 @@ class GeneralSettingsRepository extends ChangeNotifier {
   Future<void> update(GeneralSettings settings) async {
     _settings = settings;
     notifyListeners();
+    await _save();
+  }
+
+  Future<void> _save() async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString("general_settings", jsonEncode(settings.toJson()));
+    await prefs.setString("general_settings", jsonEncode(_settings.toJson()));
   }
 }
